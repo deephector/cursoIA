a01eea715c0f7cd1cb7173f95c9e6c99a8bed533
biome3 <biome3@biome3.cenicana.local>
2021-02-28 19:26:21 -0500

    add calculo guardar

---
+++ calculo.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+
+def f(x):
+    return np.sqrt(x + 2)
+
+
+
+x = np.array([-2, -1, 0, 2, 4, 6])  # Creando el vector de valores de x
+y = f(x)
+y
+
+tabla = pd.DataFrame( list(zip(x, y)), columns=['x', 'f(x)'] )
+tabla
+
+print(tabla)
